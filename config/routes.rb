c38dcc34874e28705cecac42864e6e9e460e1aa3
Igor Gushiken <igushiken@Igors-MacBook-Pro.local>
2012-05-19 00:31:43 -0300

    create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HackathonCoop12::Application.routes.draw do
+  get "users/index"
+
   #get "home/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ HackathonCoop12::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+   root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
