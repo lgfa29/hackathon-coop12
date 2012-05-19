0044038b19ec3f99600cfab313ea9ce8448f1222
Igor Gushiken <igushiken@Igors-MacBook-Pro.local>
2012-05-19 04:35:38 -0300

    Send messages

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,4 +2,14 @@ class FriendsController < ApplicationController
 	def index
 		@msg = "Ola"
 	end
+	
+	def send_message
+	  @graph = Koala::Facebook::API.new(@oauth_token)
+	  @graph.put_wall_post(params[:message], {}, 100001459536743, {})
+	end
+	
+	def write_message
+	  @graph = Koala::Facebook::API.new(@oauth_token)
+	end
+	
 end
