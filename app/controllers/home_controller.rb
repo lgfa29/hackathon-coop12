71b38a910385a4cf0d78735a029ed2d6dab43b07
Igor Gushiken <igushiken@Igors-MacBook-Pro.local>
2012-05-19 02:44:33 -0300

    First Koala Integration

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+    @graph = Koala::Facebook::API.new(@oauth_token)
+    query="SELECT name, birthday_date FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me()) 
+                                                          AND strlen(birthday_date) != 0 
+                                                          AND (substr(birthday_date, 0, 2) = '05' 
+                                                          AND substr(birthday_date, 3, 5) >= '19')  
+                                                          ORDER BY birthday_date"
+    @aniversarios = @graph.fql_query(query)
+    #@api.put_wall_post(process_result(fql))
+    #@likes = @graph.get_connections("me", "likes")
+  end
+end
