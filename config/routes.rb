4215f864c1c739f77c4a55f57057e77c484a717d
Marylia Gutierrez <maryliagutierrez@MacBook-de-Marylia-Gutierrez.local>
2012-05-19 08:22:49 -0300

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ HackathonCoop12::Application.routes.draw do
   get "users/index"
 
   #get "home/index"
-  
+  match ':controller(/:action(/:id))(.:format)'
   resources :friends
 
   # The priority is based upon order of creation:
@@ -61,5 +61,4 @@ HackathonCoop12::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-   match ':controller(/:action(/:id))(.:format)'
 end
