5a0d04f5765436d3d933bab574bb857843d6993e
Igor Gushiken <igushiken@Igors-MacBook-Pro.local>
2012-05-19 05:55:35 -0300

    sei la mais

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -5,11 +5,12 @@ class FriendsController < ApplicationController
 	
 	def send_message
 	  @graph = Koala::Facebook::API.new(@oauth_token)
-	  @graph.put_wall_post(params[:message], {}, 100001459536743, {})
+	  @graph.put_wall_post(params[:message], {}, params[:destination], {})
 	end
 	
 	def write_message
 	  @graph = Koala::Facebook::API.new(@oauth_token)
+	  @facts = Fact.where('date.mon' => Date.today.month )
 	end
 	
 end
