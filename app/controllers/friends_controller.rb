8ff8076e6178fab52df7c2ad285ae9ad62a8d919
Luiz Aoqui <lgfa29@gmail.com>
2012-05-19 00:35:06 -0300

    teste

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,5 @@
+class FriendsController < ApplicationController
+	def index
+		@msg = "Ola"
+	end
+end
---
+++ app/models/friend.rb
@@ -0,0 +1,5 @@
+class Friend
+	include Mongoid::Document
+
+	field :name, :type => String
+end
