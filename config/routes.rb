79679779bfd0c19e733e488e5d303a988b12b09f
Marylia Gutierrez <maryliagutierrez@MacBook-de-Marylia-Gutierrez.local>
2012-05-19 08:25:47 -0300

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -61,11 +61,4 @@ HackathonCoop12::Application.routes.draw do
    root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
-
-<<<<<<< HEAD
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-=======
-  
->>>>>>> 48a665f732abbc4a1a6598451004662230fe221a
 end
