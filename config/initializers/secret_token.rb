067a9e96b2d0ae15e3115f6a08083fabdeb6ebc7
Luiz Aoqui <lgfa29@gmail.com>
2012-05-18 22:24:59 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackathonCoop12::Application.config.secret_token = 'f17ef82d83000cde2c325617a702ef791afa15840b991f0b1ebbac0cc6e1852a601648399f0f63e6e95f9e296f05712833d679eb18e9289eea8991c369d05a8f'
